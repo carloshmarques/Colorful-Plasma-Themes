336d70be2aa6f0d93309c0aa75c66635bde87ad8
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2025-12-07 11:04:56 +0000

    AtualizaÃ§Ã£o cerimonial dos temas em 2025-12-07 11-04

--- updateplasma.ps1
+++ updateplasma.ps1
@@ -5,6 +5,7 @@ $TIMESTAMP = Get-Date -Format "HH-mm"
 
 $SOURCE = "$env:USERPROFILE\Documents\GitHub\main\Colorful-Plasma-Themes"
 $DEST   = "$env:USERPROFILE\Documents\GitHub\master\HydraLife\LifeCicles\Assets\Themes\Colorful-Plasma-Themes"
+$HYDRA  = "$env:USERPROFILE\Documents\GitHub\master\HydraLife"
 
 Write-Host "[HYDRA] Copiando temas de $SOURCE para $DEST..."
 robocopy $SOURCE $DEST /E /XD .git /XF sync-to-hydralife.sh update-themes.sh updateplasma.ps1 /LOG:$SOURCE\robocopy_log.txt
@@ -17,10 +18,13 @@ git commit -m "Atualização cerimonial dos temas em $DATESTAMP $TIMESTAMP"
 git push origin main
 Write-Host "[HYDRA] Commit realizado no repositório Colorful-Plasma-Themes (fork)"
 
-# 2. Atualizar referência do submódulo HydraLife
-Set-Location "$env:USERPROFILE\Documents\GitHub\master\HydraLife"
+# 2. Atualizar referência do submódulo em HydraLife
+Write-Host "[HYDRA] Atualizando referência do submódulo em HydraLife..."
+Set-Location $HYDRA
 git add LifeCicles/Assets/Themes/Colorful-Plasma-Themes
-git commit -m "Referência do submódulo Colorful-Plasma-Themes atualizada em $DATESTAMP $TIMESTAMP"
+git commit -m "[HYDRA] Referência do submódulo atualizada em $DATESTAMP $TIMESTAMP"
 git push origin master
 Write-Host "[HYDRA] Referência do submódulo atualizada no repositório HydraLife"
+
+
 
